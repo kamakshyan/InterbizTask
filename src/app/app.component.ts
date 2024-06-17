b00b19b1655d69ce7a5bd54094978551059fc394
Kamakshya Nanda <91257779+kamakshyan@users.noreply.github.com>
2024-06-17 14:02:08 +0530

    Init
    Primeng + Tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LineChartComponent } from './line-chart/line-chart.component';
+import { DataCardsComponent } from './data-cards/data-cards.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LineChartComponent,DataCardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
