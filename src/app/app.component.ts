af1dfc9f255e65a20619fe42ee2dbd11191c9d7b
Kamakshya Nanda <91257779+kamakshyan@users.noreply.github.com>
2024-06-17 14:51:51 +0530

    Added Filter components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LineChartComponent } from './line-chart/line-chart.component';
 import { DataCardsComponent } from './data-cards/data-cards.component';
+import { StateFilterComponent } from './state-filter/state-filter.component';
+import { DateFilterComponent } from './date-filter/date-filter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LineChartComponent,DataCardsComponent],
+  imports: [RouterOutlet, LineChartComponent,DataCardsComponent, StateFilterComponent, DateFilterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/state-filter/state-filter.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+
+@Component({
+  selector: 'app-state-filter',
+  standalone: true,
+  imports: [FormsModule, DropdownModule],
+  templateUrl: './state-filter.component.html',
+  styleUrl: './state-filter.component.css'
+})
+export class StateFilterComponent {
+    countries: any[] | undefined;
+    selectedCountry: string | undefined;
+    ngOnInit() {
+      this.countries = [
+          { name: 'Australia', code: 'AU' },
+          { name: 'Brazil', code: 'BR' },
+          { name: 'China', code: 'CN' },
+          { name: 'Egypt', code: 'EG' },
+          { name: 'France', code: 'FR' },
+          { name: 'Germany', code: 'DE' },
+          { name: 'India', code: 'IN' },
+          { name: 'Japan', code: 'JP' },
+          { name: 'Spain', code: 'ES' },
+          { name: 'United States', code: 'US' }
+      ];
+  }
+}
